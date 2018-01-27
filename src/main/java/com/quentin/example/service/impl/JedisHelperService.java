7cd51b0ba55e6e615878378970898c823e2d2a1e
unclequentin <qundongni@qq.com>
2018-01-28 00:53:52 +0800

    注释掉无用引用

--- src/main/java/com/quentin/example/service/impl/JedisHelperService.java
+++ src/main/java/com/quentin/example/service/impl/JedisHelperService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.JedisCluster;
 
-import javax.annotation.Resource;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
